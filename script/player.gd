f7736f6e9029e97303728595ab5c13d2e6c80e8b
Haoyan Li <haoyanli234@gmail.com>
2026-06-20 16:47:11 -0400

    Added dashes to the movement system and reworked it to work with those

--- script/player.gd
+++ script/player.gd
@@ -1,26 +1,45 @@
 extends CharacterBody2D
 
 const SPEED = 200
-const ACCELERATION = 20
-const FRICTION = 20
+const DASH_LENGTH = 5
+const DASH_FRICTION = 40
 
-var mouse_events: Array
+var mouse_events: Array = []
+var dash_velocity: Vector2
+var base_move_velocity: Vector2
 
 func _physics_process(_delta: float) -> void:
+	
+	# Basic movement
 	var direction = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN").normalized()
 	if direction:
-		velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION)
-		velocity.y = move_toward(velocity.y, direction.y * SPEED, ACCELERATION)
+		base_move_velocity.x = direction.x * SPEED
+		base_move_velocity.y = direction.y * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION)
-		velocity.y = move_toward(velocity.y, 0, FRICTION) 
+		base_move_velocity.x = 0
+		base_move_velocity.y = 0
 	
-	# Mouse input is read from variable
+	# Click-based movement
+	# Read from array of deferred mouse events
+	if len(mouse_events) != 0:
+		for event in mouse_events:
+			# Movement vector for dashing
+			var move_direction = Vector2(event.position.x - (get_viewport().size.x)/2, event.position.y - (get_viewport().size.y)/2)
+			var move_vector = move_direction.normalized() * clamp(move_direction.length(),10, 200) * DASH_LENGTH
+			# Attack on left click
+			# if !$AtkCooldown.is_stopped() and event.button_mask == 1:
+				
+			dash_velocity = move_vector
+			mouse_events.erase(event)
+			
+	dash_velocity = dash_velocity.move_toward(Vector2(0,0), DASH_FRICTION)
 	
+	velocity = base_move_velocity + dash_velocity
 	move_and_slide()
 	
 func _input(event):
 	# Catches all mouse events but defers them to physics process
 	if event is InputEventMouseButton:
-		pass
+		if event.button_mask != 0:
+			mouse_events.append(event)
 	
