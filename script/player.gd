a1c3052a0ac9fda36a226438927b914ee28a93f0
Haoyan Li <haoyanli234@gmail.com>
2026-06-25 17:30:26 -0400

    Added basic behavior to attacks/dashes. Attacks recharge faster and can have shorter move distances while dashes always move further and *will* have invincibility. Also implented a state machine to keep track of those. At the moment, the dash state extends till the timer is up, which can be a bit after the actual movement ends. That can feel sluggish, so I'll change that later.

--- script/player.gd
+++ script/player.gd
@@ -8,8 +8,10 @@ var mouse_events: Array = []
 var dash_velocity: Vector2
 var base_move_velocity: Vector2
 
+# important state thing
+var state: String = "idle"
+
 func _physics_process(_delta: float) -> void:
-	
 	# Basic movement
 	var direction = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN").normalized()
 	if direction:
@@ -24,22 +26,45 @@ func _physics_process(_delta: float) -> void:
 	if len(mouse_events) != 0:
 		for event in mouse_events:
 			# Movement vector for dashing
-			var move_direction = Vector2(event.position.x - (get_viewport().size.x)/2, event.position.y - (get_viewport().size.y)/2)
-			var move_vector = move_direction.normalized() * clamp(move_direction.length(),10, 200) * DASH_LENGTH
-			# Attack on left click
-			# if !$AtkCooldown.is_stopped() and event.button_mask == 1:
-				
+			var move_direction: Vector2 = Vector2(event.position.x - (get_viewport().size.x)/2, event.position.y - (get_viewport().size.y)/2)
+			var move_length: Array # [length_min, length_max]
+			# Cooldown timers are for until next of either action, so after dashing you can only dash or move again after that amount of time
+			if $AtkCooldown.is_stopped() and $DashCooldown.is_stopped():
+				# Attack on left click
+				if event.button_mask == 1:
+					$AtkCooldown.start()
+					move_length = [10, 200]
+					state = "attack"
+				# Dash on right click
+				elif event.button_mask == 2:
+					$DashCooldown.start()
+					move_length = [300,400]
+					state = "dash"
+			else:
+				mouse_events.erase(event)
+				break
+			# Convert vector to the actual movement (with velocity & movement cap)
+			var move_vector: Vector2 = move_direction.normalized() * clamp(move_direction.length(), move_length[0], move_length[1]) * DASH_LENGTH
 			dash_velocity = move_vector
+			# Toss event because it's been processed
 			mouse_events.erase(event)
-			
+	
+	# Decelerate dash
 	dash_velocity = dash_velocity.move_toward(Vector2(0,0), DASH_FRICTION)
 	
+	# Movements are separate to make dashing have a more expected behavior
 	velocity = base_move_velocity + dash_velocity
 	move_and_slide()
+	
+	print(state)
 	
 func _input(event):
 	# Catches all mouse events but defers them to physics process
 	if event is InputEventMouseButton:
 		if event.button_mask != 0:
 			mouse_events.append(event)
+			
+func _ready():
+	$AtkCooldown.connect("timeout", func () : state = "idle")
+	$DashCooldown.connect("timeout", func () : state = "idle")
 	
